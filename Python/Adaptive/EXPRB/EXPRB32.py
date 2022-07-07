7f924d2d0d956f9489e7f4274c3b63b1ede94bbd
Pranab Deka <61944428+Pranab-JD@users.noreply.github.com>
2022-07-07 16:02:05 +0200

    Add files via upload

--- Python/Adaptive/EXPRB/EXPRB32.py
+++ Python/Adaptive/EXPRB/EXPRB32.py
@@ -2,17 +2,17 @@ from Leja_Interpolation import *
 
 ################################################################################################
 
-def EXPRB32(u, dt, RHS_func, c, Gamma, rel_tol, Real_Imag_Leja):
+def EXPRB32(u, dt, RHS_function, c, Gamma, tol, Real_Imag):
     """
     Parameters
     ----------
 	u               : 1D vector u (input)
 	dt              : Step size
-	RHS_func	    : RHS function
+	RHS_function	: RHS function
     c               : Shifting factor
     Gamma           : Scaling factor
-    rel_tol         : Accuracy of the polynomial so formed
-    Real_Imag_Leja  : 0 - Real, 1 - Imaginary
+    tol             : Accuracy of the polynomial so formed
+    Real_Imag       : 0 - Real, 1 - Imaginary
 
     Returns
     -------
@@ -23,48 +23,63 @@ def EXPRB32(u, dt, RHS_func, c, Gamma, rel_tol, Real_Imag_Leja):
     """
 
     ### Interpolate on either real Leja or imaginary Leja points.
-    if Real_Imag_Leja == 0:
+    if Real_Imag == 0:
         Leja_phi = real_Leja_phi
-    elif Real_Imag_Leja == 1:
+    elif Real_Imag == 1:
         Leja_phi = imag_Leja_phi
     else:
         print("Error!! Choose 0 for real or 1 for imaginary Leja points.")
-
-    epsilon = 1e-7
     
     ### RHS of PDE at u
-    f_u = RHS_func(u)
+    f_u = RHS_function(u)
+    
+    ### Function to compute the nonlinear remainder at stage 'y'
+    def Nonlinear_remainder(y):
+        
+        epsilon = 1e-7
+        
+        ### J(u) * y
+        Linear_y = (RHS_function(u + (epsilon * y)) - f_u)/epsilon
+
+        ### F(y) = f(y) - (J(u) * y)
+        Nonlinear_y = RHS_function(y) - Linear_y
+        
+        return Nonlinear_y
 
     ############## --------------------- ##############
 
-    ### Internal stage 1 
-    a_n_f, rhs_calls_1 = Leja_phi(u, dt, RHS_func, f_u, c, Gamma, phi_1, rel_tol)
-    a_n = u + (a_n_f * dt)
+    ### Internal stage 1; interpolation 1
+    u_flux, rhs_calls_1, convergence = Leja_phi(u, dt, RHS_function, f_u*dt, c, Gamma, phi_1, tol)
+    
+    ## If it does not converge, return (try with smaller dt)
+    if convergence == 0:
+
+        u_exprb2 = u;
+        u_exprb3 = 2.1*u;
+        num_rhs_calls = rhs_calls_1;
+
+        return u_exprb2, u_exprb3, num_rhs_calls
 
     ### 2nd order solution
-    u_exprb2 = a_n
+    u_exprb2 = u + u_flux
 
     ############## --------------------- ##############
 
-    ### J(u) * u
-    Linear_u = (RHS_func(u + (epsilon * u)) - f_u)/epsilon
+    ### Nonlinear remainder at u
+    Nonlinear_u = Nonlinear_remainder(u)
 
-    ### F(u) = f(u) - (J(u) * u)
-    Nonlin_u = f_u - Linear_u
-
-    ### J(u) * a
-    Linear_a = (RHS_func(u + (epsilon * a_n)) - f_u)/epsilon
-
-    ### F(a) = f(a) - (J(u) * a)
-    Nonlin_a = RHS_func(a_n) - Linear_a
+    ### Nonlinear remainder at u_exprb2
+    Nonlinear_a = Nonlinear_remainder(u_exprb2)
 
     ############## --------------------- ##############
 
+    ### Interpolation 2 (Nonlinear remainders always converge)
+    u_nl_3, rhs_calls_2, convergence = Leja_phi(u, dt, RHS_function, 2*(Nonlinear_a - Nonlinear_u)*dt, c, Gamma, phi_3, tol)
+    
     ### 3rd order solution
-    u_3, rhs_calls_2 = Leja_phi(u, dt, RHS_func, 2*(Nonlin_a - Nonlin_u), c, Gamma, phi_3, rel_tol)
-    u_exprb3 = u_exprb2 + (u_3 * dt)
+    u_exprb3 = u_exprb2 + u_nl_3
 
     ## Proxy of computational cost
-    num_rhs_calls = rhs_calls_1 + rhs_calls_2 + 4
+    num_rhs_calls = rhs_calls_1 + rhs_calls_2 + 5
 
     return u_exprb2, u_exprb3, num_rhs_calls
