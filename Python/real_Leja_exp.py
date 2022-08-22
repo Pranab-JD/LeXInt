38ea2630c780631781e4c3a7d28839b34d307289
Pranab Jyoti Deka <pranab.deka@uibk.ac.at>
2022-08-22 10:55:34 +0200

    Update real_Leja_exp.py

--- Python/real_Leja_exp.py
+++ Python/real_Leja_exp.py
@@ -21,49 +21,40 @@ def real_Leja_exp(u, dt, RHS_function, c, Gamma, Leja_X, tol):
 
     """
     
-    s = 1                       # Number of substeps
-    rhs_calls = 0               # Counter for # of Leja points used/RHS calls
-    max_Leja_pts = len(Leja_X)  # Max # of Leja points
+    ### Max # of Leja points
+    max_Leja_pts = len(Leja_X)  
     
     ### Matrix exponential (scaled and shifted); scaling down of c and Gamma (i.e. largest and smallest eigenvalue) by dt 
-    matrix_exponential = np.exp((c + Gamma*Leja_X) * dt/s)
+    matrix_exponential = np.exp((c + Gamma*Leja_X) * dt)
     
     ### Compute the polynomial coefficients
     coeffs = Divided_Difference(Leja_X, matrix_exponential) 
     
     ### ------------------------------------------------------------------- ###
     
-    ### Form the polynomial
-    for substeps in range(0, s):                        
-                
-        ### p_0 term
-        if substeps > 0:
-            y = polynomial.copy()
-            polynomial = coeffs[0] * polynomial 
-        else:
-            y = u.copy()
-            polynomial = coeffs[0] * u      
+     ### Form the polynomial, p_0 term
+    y = u.copy()
+    polynomial = coeffs[0] * u      
 
-        ### p_1, p_2, ...., p_n terms; iterate until polynomial converges
-        for ii in range(1, max_Leja_pts):
+    ### p_1, p_2, ...., p_n terms; iterate until polynomial converges
+    for ii in range(1, max_Leja_pts):
 
-            ### y = y * ((z - c)/Gamma - Leja_X)
-            y = (RHS_function(y)/Gamma) + (y * (-c/Gamma - Leja_X[ii - 1]))
+        ### y = y * ((z - c)/Gamma - Leja_X)
+        y = (RHS_function(y)/Gamma) + (y * (-c/Gamma - Leja_X[ii - 1]))
 
-            ### Error estimate
-            poly_error = np.mean(abs(y)) * abs(coeffs[ii])
-            
-            ### Add new term to the polynomial
-            polynomial = polynomial + (coeffs[ii] * y)
+        ### Error estimate
+        poly_error = np.norm(y) * abs(coeffs[ii])
 
-            ### If new term to be added < tol, break loop; safety factor = 0.1
-            if  poly_error < 0.1*tol:
-                print("# Leja points: ", ii)
-                rhs_calls = rhs_calls + ii
-                break
+        ### Add new term to the polynomial
+        polynomial = polynomial + (coeffs[ii] * y)
 
-            if ii == max_Leja_pts - 1:
-                print("Warning!! Max. # of Leja points reached!!")
-                break
+        ### If new term to be added < tol, break loop; safety factor = 0.1
+        if  poly_error < 0.1*tol:
+            print("# Leja points: ", ii)
+            break
+
+        if ii == max_Leja_pts - 1:
+            print("Warning!! Max. # of Leja points reached!!")
+            break
 
     return polynomial, rhs_calls
