1d7071be5136bde6938519ce7a5360c9f669f7ce
Pranab Jyoti Deka <pranab.deka@uibk.ac.at>
2022-11-21 12:06:51 +0100

    Update EXPRB53s3.py

--- Python/Constant/EXPRB53s3.py
+++ Python/Constant/EXPRB53s3.py
@@ -26,7 +26,7 @@ def EXPRB53s3(u, dt, RHS_function, c, Gamma, Leja_X, tol, Real_Imag):
     num_rhs_calls   : # of RHS calls
     
     Reference:
-        V. T. Luan, A. Ostermann, Exponential rosenbrock methods of order five — construction, analysis and numerical comparisons, J. Comput. Appl. Math. 255 (2014) 417-431. 
+        V. T. Luan, A. Ostermann, Exponential rosenbrock methods of order five - construction, analysis and numerical comparisons, J. Comput. Appl. Math. 255 (2014) 417-431. 
         doi:10.1016/j.cam.2013.04.041.
 
     """
