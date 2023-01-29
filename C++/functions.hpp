19463d5dab59679f4342899cf9c4e29117631c2b
Pranab Jyoti Deka <pranab.deka@uibk.ac.at>
2023-01-29 14:40:27 +0100

    Update functions.hpp

--- C++/functions.hpp
+++ C++/functions.hpp
@@ -26,6 +26,7 @@ void sub_timespec(struct timespec t1, struct timespec t2, struct timespec *td)
 {
     td->tv_nsec = t2.tv_nsec - t1.tv_nsec;
     td->tv_sec  = t2.tv_sec - t1.tv_sec;
+
     if (td->tv_sec > 0 && td->tv_nsec < 0)
     {
         td->tv_nsec += NS_PER_SECOND;
