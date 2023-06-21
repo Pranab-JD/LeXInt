d8e1ae53a744636d2329281cbcbd58a6fa19c8c6
Pranab J. Deka <pranab.deka@uibk.ac.at>
2023-06-21 18:52:25 +0200

    Update error_check.hpp

--- CUDA/error_check.hpp
+++ CUDA/error_check.hpp
@@ -1,30 +1,17 @@
 #pragma once
 
-#ifdef __CUDACC__
-
 using namespace std;
 
-//* This piece of code has been taken from 
-//* https://stackoverflow.com/questions/14038589/what-is-the-canonical-way-to-check-for-errors-using-the-cuda-runtime-api
+#ifdef __CUDACC__
 
-// #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-// inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
-// {
-//    if (code != cudaSuccess) 
-//    {
-//       fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-//       if (abort) exit(code);
-//    }
-// }
-
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
-{
-   if (code != cudaSuccess) 
+   #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+   inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
    {
-      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-      if (abort) exit(code);
-   }  
-}
+      if (code != cudaSuccess) 
+      {
+         fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+         if (abort) exit(code);
+      }  
+   }
 
 #endif
