7cb5ac7c22af4688770063cff80614221b7731af
Pranab J. Deka <pranab.deka@uibk.ac.at>
2023-07-23 20:33:17 +0200

    Augmented matrix

--- Python/linear_phi.py
+++ Python/linear_phi.py
@@ -1,17 +1,70 @@
 import numpy as np
-from A_tilde import A_tilde
-from real_Leja_linear_exp import *
+from real_Leja_linear_exp import real_Leja_linear_exp
+
+def A_tilde(A, B, v):
+    """
+    Form the augmented matrix.
+
+    Args:
+        A (function handle R^n -> R^n)
+        B (matrix, n*p)
+        v (vector, n+p)
+
+    Returns:
+        y (vector) : A_tilde . v, where A_tilde = [A B; 0 K] and K = [0 I; 0 0]
+    
+        
+    Reference: 
+
+        R.B. Sidje, Expokit: A Software Package for Computing Matrix Exponentials, ACM Trans. Math. Softw. 24 (1) (1998) 130 - 156.
+        doi:10.1145/285861.285868
+
+    """
+
+    [p, n] = np.shape(B)
+    
+    y = np.concatenate([    A(v[0:n]).reshape(1, n) + np.dot(v[n:n+p].reshape(1, p), B),
+                            [v[n+1:n+p]],
+                            np.array([0]).reshape(1, 1)], axis = 1)
+    
+    return y.reshape(np.shape(y)[1])
+
 
 def linear_phi(interp_vector, dt, Jacobian_vector, c, Gamma, Leja_X, tol):
+    """
+    Evaluates a linear combinaton of the phi functions as the 
+    exponential of an augmented matrix.
+    
+     polynomial[0:n] = phi_0(A) u(:, 1) + phi_1(A) u(:, 2) + ... + phi_p(A) u(:, p+1)
+
+    Args:
+        interp_vector (vector n*(p+1))      : Vector to evaluated/interpolated
+        dt (double)                         : Step size
+        Jacobian_vector (function handle)   : Jacobian-vector product (multiplied by dt)
+        c (double)                          : Shifting factor
+        Gamma (double)                      : Scaling factor
+        Leja_X (vector)                     : Array of Leja points
+        tol (double)                        : Accuracy of the polynomial so formed
+
+    Returns:
+         polynomial[0:n] (vector)           : Linear combinaton of the phi functions
+    
+
+    Reference: 
+
+        R.B. Sidje, Expokit: A Software Package for Computing Matrix Exponentials, ACM Trans. Math. Softw. 24 (1) (1998) 130 - 156.
+        doi:10.1145/285861.285868
+
+    """
     
     [m, n] = np.shape(interp_vector)
     B = np.flipud(interp_vector[1:m])
     p = m - 1
 
     Atx = lambda x: A_tilde(Jacobian_vector, B, x)
-
+    
     v = np.concatenate([interp_vector[0], np.zeros(p-1), [1]])
 
-    polynomial = real_Leja_exp(v, dt, Atx, c, Gamma, Leja_X, tol)
+    polynomial, rhs_calls = real_Leja_linear_exp(v, dt, Atx, c, Gamma, Leja_X, tol)
     
-    return polynomial[0]
+    return polynomial[0:n], rhs_calls
--- Python/real_Leja_linear_exp.py
+++ Python/real_Leja_linear_exp.py
@@ -1,7 +1,9 @@
 import numpy as np
 from Divided_Difference import Divided_Difference
 
-def real_Leja_exp(u, dt, RHS_function, c, Gamma, Leja_X, tol):
+from datetime import datetime
+
+def real_Leja_linear_exp(u, dt, RHS_function, c, Gamma, Leja_X, tol):
     """
     Computes the polynomial interpolation of matrix exponential applied to 'u' at real Leja points.
 
@@ -37,37 +39,48 @@ def real_Leja_exp(u, dt, RHS_function, c, Gamma, Leja_X, tol):
     max_Leja_pts = len(Leja_X)                                    #* Max number of Leja points  
     y = u.copy()                                                  #* To avoid changing 'interp_function'
 
+    substeps = 4
+    
     ###? Matrix exponential (scaled and shifted)
-    matrix_exponential = np.exp(dt * (c + Gamma*Leja_X))
+    matrix_exponential = np.exp(dt/substeps * (c + Gamma*Leja_X))
 
     ###? Compute polynomial coefficients
-    poly_coeffs = Divided_Difference(Leja_X, matrix_exponential) 
+    poly_coeffs = Divided_Difference(Leja_X, matrix_exponential)
 
-    ###? Form the polynomial: p_0 term
-    polynomial = poly_coeffs[0] * u
+    time = datetime.now()
 
-    ###? p_1, p_2, ...., p_n terms; iterate until converges
-    for ii in range(1, max_Leja_pts):
+    for s in range(0, substeps):
+
+        ###? Form the polynomial: p_0 term
+        if s == 0:
+            polynomial = poly_coeffs[0] * y
+        else:
+            y = polynomial.copy()
+            polynomial = poly_coeffs[0] * y
         
-        ###? y = y * ((z - c)/Gamma - Leja_X)
-        y = (RHS_function(y)/(dt*Gamma)) + (y * (-c/Gamma - Leja_X[ii - 1]))
+        ###? p_1, p_2, ...., p_n terms; iterate until converges
+        for ii in range(1, max_Leja_pts):
 
-        ###? Error estimate; poly_error = |coeffs[nn]| ||y||
-        poly_error = np.linalg.norm(y) * abs(poly_coeffs[ii])
-        
-        ###? Add the new term to the polynomial
-        polynomial = polynomial + (poly_coeffs[ii] * y)
+            ###? y = y * ((z - c)/Gamma - Leja_X)
+            y = (RHS_function(y)/(dt*Gamma*substeps)) + (y * (-c/Gamma - Leja_X[ii - 1]))
 
-        ###? If new term to be added < tol, break loop; safety factor = 0.25
-        if  poly_error < (0.25*tol*np.linalg.norm(polynomial) + tol):
-            print("Converged! # of Leja points used (exp): ", ii)
-            break
+            ###? Error estimate; poly_error = |coeffs[nn]| ||y||
+            poly_error = np.linalg.norm(y) * abs(poly_coeffs[ii])
+            
+            ###? Add the new term to the polynomial
+            polynomial = polynomial + (poly_coeffs[ii] * y)
 
-        ###! Warning flags
-        if ii == max_Leja_pts - 1:
-            print("Warning!! Max. # of Leja points reached without convergence!!")
-            print("Max. Leja points currently set to", max_Leja_pts)
-            print("Try increasing the number of Leja points. Max available: 10000.\n")
-            break
+            ###? If new term to be added < tol, break loop; safety factor = 0.25
+            if  poly_error < (tol*np.linalg.norm(polynomial) + tol):
+                # print("Converged! # of Leja points used (exp): ", ii)
+                print(datetime.now() - time, "  ", ii)
+                break
 
-    return polynomial, ii
+            ###! Warning flags
+            if ii == max_Leja_pts - 1:
+                print("Warning!! Max. # of Leja points reached without convergence!!")
+                print("Max. Leja points currently set to", max_Leja_pts)
+                print("Try increasing the number of Leja points. Max available: 10000.\n")
+                break
+
+    return polynomial, ii*substeps
