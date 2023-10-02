9ee925aa7314decfc99b083a3ade9f5b30eb05fb
Pranab J. Deka <pranab.deka@uibk.ac.at>
2023-10-02 14:20:44 +0200

    Update run_cuda.sh

--- CUDA/Test/run_cuda.sh
+++ CUDA/Test/run_cuda.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 #SBATCH -p a100
-#SBATCH -o cuda_.out
+#SBATCH -o cuda_results.out
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -t 51:00:00
+#SBATCH -t 10:00:00
 
+## Load available CUDA module
 module load cuda/11.2
+
+## Compile using nvcc (cuBLAS required)
 nvcc test_2D.cu -O3 -o executable -lcublas
 
 printf "Compiled successfully... Yaayy!\n"
@@ -18,7 +21,7 @@ printf "Starting simulations ....\n"
 ## E.g.: Say, domain = 1024 * 1024 (2^10 * 2^10), dt = 2*dt_cfl, tol = 1e-8, and T_final = 1.0, then run
 ## E.g.: ./executable 10 2 1e-8 1.0
 
-./executable 13 5 1e-10 5e-5
+./executable 9 5 1e-10 5e-5
 
-printf "Completed simualtions ....\n"
+printf "Completed simulations ....\n"
 printf "====================================================\n"
