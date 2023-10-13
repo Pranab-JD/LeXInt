b796fe95990b7f7168a1748ce2cf0f45ddc7d4ab
Pranab J. Deka <pranab.deka@uibk.ac.at>
2023-10-13 11:14:16 +0200

    Jacobian is computed using forward finite difference

--- Python/Jacobian.py
+++ Python/Jacobian.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-###? Jacobian_vector = (RHS(u + epsilon*v) - RHS(u - epsilon*v))/(2*epsilon)
+###? Jacobian_vector = (RHS(u + epsilon*v) - RHS(u))/epsilon
 def Jacobian(RHS, u, v, *args):
     
-    ###* epsilon has to be normalised to RHS(u)
-    epsilon = 1e-7 * np.linalg.norm(RHS(u, *args))
+    ###* epsilon is normalised to norm(u)
+    epsilon = 1e-7 * np.linalg.norm(u)
     
-    ###* J(u) * y = (RHS(u + epsilon*v) - RHS(u - epsilon*v))/(2*epsilon)
-    Jac_vec = (RHS(u + (epsilon * v), *args) - RHS(u - (epsilon * v), *args))/(2 * epsilon)
+    ###* J(u) * v = (RHS(u + epsilon*v) - RHS(u))/epsilon
+    Jacobian_vector = (RHS(u + (epsilon * v), *args) - RHS(u, *args))/epsilon
     
-    return Jac_vec
+    return Jacobian_vector
