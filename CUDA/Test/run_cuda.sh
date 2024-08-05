accb732ed1a258b3598780bd3a5ec61351426335
PJD <pranab.deka@kuleuven.be>
2024-08-05 16:47:41 +0200

    Updated gitignore, deleted unnecessary files

--- CUDA/Test/run_cuda.sh
+++ CUDA/Test/run_cuda.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
-
-#SBATCH -p tv
-#SBATCH -o cuda_.out
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -t 10:00:00
-
-## Load available CUDA module
-module load cuda/11.2
+#SBATCH --account=EUHPC_D14_051
+#SBATCH --partition=boost_usr_prod
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=00:20:00
+#SBATCH --job-name=lexint
+#SBATCH -o build/cuda_bw.out
 
 ## Compile using nvcc (cuBLAS required)
-nvcc test_2D.cu -O3 -o executable -lcublas
-
-printf "Compiled successfully... Yaayy!\n"
+# nvcc ../test_2D.cu -O3 -o lexint_cuda -lcublas
 
 printf "====================================================\n"
-printf "Starting 1st set ....\n"
+date
 
 ## E.g.: Say, domain = 1024 * 1024 (2^10 * 2^10), dt = 2*dt_cfl, tol = 1e-8, and T_final = 1.0, then run
 ## E.g.: ./executable 10 2 1e-8 1.0
 
-./executable 10 5 1e-10 5e-5
+# ./lexint_cuda 12 1 1e-12 2e-6
+# ./lexint_cuda 12 10 1e-12 2e-5
+# ./lexint_cuda 12 100 1e-12 2e-4
+
+# ./lexint_cuda 13 1 1e-12 2e-7
+# ./lexint_cuda 13 10 1e-12 2e-6
+# ./lexint_cuda 13 100 1e-12 2e-5
+
+# ./lexint_cuda 14 1 1e-12 2e-8
+./lexint_cuda 14 100 1e-12 2e-7
+# ./lexint_cuda 14 100 1e-12 2e-6
 
 printf "Completed simulations ....\n"
+date
 printf "====================================================\n"
