3d406ad9a5a66c2a2d8a325c4edc4b45c5d56066
pjd <pranab.deka@kuleuven.be>
2024-10-03 11:23:04 +0200

    Unnecessary files commited, needs to be deleted

--- CUDA/Test/run_cpp.sh
+++ CUDA/Test/run_cpp.sh
@@ -23,7 +23,7 @@ printf "Starting 1st set ....\n"
 ## E.g.: Say, domain = 1024 * 1024 (2^10 * 2^10), dt = 2*dt_cfl, tol = 1e-8, and T_final = 1.0, then run
 ## E.g.: ./executable 10 2 1e-8 1.0
 
-./executable 9 2 1e-10 5e-4
+./executable 9 2 1e-10 1e-4
 
 printf "Completed simulations ....\n"
 printf "====================================================\n"
