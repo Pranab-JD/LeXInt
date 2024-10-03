36d6a97af155ea3acd88af6391d366d6f3c27e1b
pjd <pranab.deka@kuleuven.be>
2024-10-03 10:56:32 +0200

    Testing substeps for augmented matrix

--- Python/real_Leja_linear_exp.py
+++ Python/real_Leja_linear_exp.py
@@ -1,7 +1,8 @@
 import numpy as np
+from bisect import bisect_left
 from Divided_Difference import Divided_Difference
 
-def real_Leja_linear_exp(u, T_f, substeps, RHS_function, integrator_coeff, c, Gamma, Leja_X, tol):
+def real_Leja_linear_exp(u, T_f, init_subs, RHS_function, integrator_coeff, c, Gamma, Leja_X, tol):
     """
     Computes the polynomial interpolation of matrix exponential applied to 'u' at real Leja points.
 
@@ -38,13 +39,14 @@ def real_Leja_linear_exp(u, T_f, substeps, RHS_function, integrator_coeff, c, Ga
     ###? Initialize parameters and arrays
     y = u.copy()                                                  #* To avoid changing 'u'
     y_backup = u.copy()                                           #* Backup y - To avoid changing 'u'
+    dt = T_f/init_subs                                            #* Initial substep size
+    dt_backup = dt                                                #* To be used if dt is increased by large value
     
     max_Leja_pts = len(Leja_X)                                    #* Max number of Leja points
-    dt = T_f/substeps                                             #* Initial substep size
-    time_elapsed = 0                                              #* Counter for time elapsed
-    subs = 1                                                      #* Counter for number of substeps
-    convergence = 0                                               #* Check for convergence
+    time_elapsed = 0                                              #* Simulation time elapsed                                          
+    substeps = 0                                                  #* Counter for number of substeps
     total_iters = 0                                               #* Counter for Leja iterations
+    convergence = [0]                                             #* Check for convergence
     
     ###! Time loop
     while time_elapsed < T_f:
@@ -53,22 +55,20 @@ def real_Leja_linear_exp(u, T_f, substeps, RHS_function, integrator_coeff, c, Ga
         poly_error = np.zeros(max_Leja_pts);                            
         
         ###* Adjust final time substep
-        if abs(T_f - time_elapsed) < 1e-12:
-            break
-        elif time_elapsed + dt > T_f:
+        if time_elapsed + dt > T_f:
             dt = T_f - time_elapsed
         
         ###? Compute polynomial coefficients
         poly_coeffs = Divided_Difference(Leja_X, np.exp(integrator_coeff * dt * (c + Gamma*Leja_X)))
-        
+            
         ###? Set y = polynomial; save y_backup (same dt)
-        if convergence == 1:
+        if convergence[-1] == 1:
             
             y = polynomial;
             y_backup = polynomial;
 
         ###? Set 'y' to previous value (reduce dt)
-        elif convergence == 0:
+        elif convergence[-1] == 0:
 
             y = y_backup;
             
@@ -88,33 +88,40 @@ def real_Leja_linear_exp(u, T_f, substeps, RHS_function, integrator_coeff, c, Ga
             poly_error[ii] = np.linalg.norm(y) * abs(poly_coeffs[ii])
             
             ###! Warning: Check for diverging values, if so, restart iteration with smaller dt
-            if ii == max_Leja_pts - 1 or poly_error[ii] > 1e3:
+            if ii == max_Leja_pts - 1 or poly_error[ii] > 1e10:
 
-                print("Step size: ", dt)
-                print("Computations wasted: ", ii)
-
+                print("Computations wasted: ", ii, " dt = ", dt)
+                
                 ###* Update parameters
-                dt = 0.5 * dt
-                subs = np.ceil(T_f/dt)
-                convergence = 0
+                convergence.append(0)
                 total_iters = total_iters + ii
 
+                ###? Check whether or not this is one of the initial stages
+                if substeps != 0:
+                    ###* If not, slowly decrease dt (since Leja converged for previous dt, we are in the vicinity)
+                    dt = 0.95*dt_backup
+                else:
+                    ###* If yes, drastically decrese dt (get to a value of dt such that Leja converges)
+                    dt = 0.15 * dt
                 break
 
             ###? If new term to be added < tol, break loop
             if  poly_error[ii] < (tol*np.linalg.norm(polynomial) + tol):
                 
-                print()
-                print("Converged! # of Leja points used (exp): ", ii)
-                print()
+                # print("Converged! # of Leja points used (exp): ", ii, " dt = ", dt)
                 
+                ###* Update parameters
+                convergence.append(1)
+                dt_backup = dt
                 time_elapsed = time_elapsed + dt
                 total_iters = total_iters + ii
-                subs = max(substeps, subs)
-                convergence = 1
-                
-                dt = 1.1*dt
+                substeps = substeps + 1
 
+                ###? If simulations never failed to converged, increase dt
+                if any(conv == 0 for conv in convergence[bisect_left(convergence, 1):-1]) == False:
+                    dt = 1.002*dt                    
+                
                 break
 
-    return polynomial, total_iters, subs
+    # print(substeps)
+    return polynomial, total_iters, substeps
